aeb7182983bb16f59358eba38be9455809cea144
Elaman Imashov <elaman@outlook.com>
2019-04-29 21:47:18 +0600

    Use photo in the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,54 @@
 import React from "react"
+import Img from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
 
 import classes from "../styles/header.module.css"
 
-const Header = ({ children, height, backgroundImage, backgroundPosition }) => {
+const Header = ({ children, height, photo }) => {
   const style = {}
-
-  if (backgroundImage) {
-    style.backgroundImage = `url(${backgroundImage})`
-    style.backgroundSize = 'cover'
-    style.backgroundPosition = backgroundPosition ? backgroundPosition : 'center'
-  }
 
   if (height) {
     style.height = `${height}vh`
   }
 
   return (
-    <h1 className={classes.Header} style={style}>
-      {children}
-    </h1>
+    <StaticQuery
+      query={graphql`
+        query {
+          photos: allFile(filter: {relativePath: {regex: "/photos/" }}) {
+            edges {
+              node {
+                childImageSharp { fluid { ...GatsbyImageSharpFluid } }
+              }
+            }
+          }
+        }        
+      `}
+      render={data => {
+        let photoOutput = null
+
+        const photoElement = data.photos.edges.find(element => {
+          return (element.node.childImageSharp.fluid.src.split('/').pop() === photo)
+        })
+        if (photoElement)  {
+          photoOutput = (
+            <Img
+              style={{ position: 'absolute' }}
+              className={classes.Photo}
+              fluid={photoElement.node.childImageSharp.fluid} />
+          )
+        }
+
+        return (
+          <header
+            className={classes.Header}
+            style={style}>
+            {photoOutput}
+            <h1>{children}</h1>
+          </header>
+        )
+      }}
+    />
   )
 }
 
