e8731e5be40919015b64826080f2ed31622c0dbb
Elaman Imashov <elaman@outlook.com>
2019-04-29 14:58:39 +0600

    Add drawer styling

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -4,17 +4,19 @@ import Backdrop from "./backdrop"
 import Logo from "./logo"
 import Navigation from "./navigation"
 
+import classes from "../styles/drawer.module.css"
+
 const Drawer = ({ state, stateToggleHandler }) => {
   return (
-    <div>
+    <div className={classes.Drawer}>
       <Backdrop
         state={state}
         toggleHandler={stateToggleHandler} />
-      <div>
-        <div>
+      <div className={[classes.Content, state ? classes.Open : classes.Closed].join(' ')}>
+        <div className={classes.Logo}>
           <Logo />
         </div>
-        <div>
+        <div className={classes.Navigation}>
           <Navigation />
         </div>
       </div>
