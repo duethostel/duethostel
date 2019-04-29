8bee635d9c7bda1a10b8ff154fbf0dac33625b4d
Elaman Imashov <elaman@outlook.com>
2019-04-29 14:55:41 +0600

    Add drawer toggle styling

--- src/components/drawerToggle.js
+++ src/components/drawerToggle.js
@@ -1,8 +1,10 @@
 import React from "react"
 
+import classes from "../styles/drawerToggle.module.css"
+
 function DrawerToggle({ stateToggleHandler }) {
   return (
-    <button onClick={stateToggleHandler}>
+    <button className={classes.DrawerToggle} onClick={stateToggleHandler}>
       <div></div>
       <div></div>
       <div></div>
