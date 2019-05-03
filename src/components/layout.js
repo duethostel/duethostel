0d5b26b3167f41b268643c70654e8a9033cfe9d1
Elaman Imashov <elaman@outlook.com>
2019-05-03 23:14:12 +0600

    Use newly uploaded fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import { StaticQuery, graphql } from "gatsby"
 import Toolbar from "./toolbar"
 import Drawer from "./drawer"
 
+import "../styles/fonts.css"
 import "../styles/styles.css"
 import classes from "../styles/layout.module.css"
 
