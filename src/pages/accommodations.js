2581d81f2947e01b2b0cffd8f3acda298ae3bd71
Elaman Imashov <elaman@Elamans-MacBook-Pro.local>
2019-05-04 00:16:33 +0600

    Implement proper syntax for pages

--- src/pages/accommodations.js
+++ src/pages/accommodations.js
@@ -1,26 +1,22 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from "../components/header"
 
-const AccommodationsPage = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "photos/accommodations.jpg" }) {
-          childImageSharp { fluid { ...GatsbyImageSharpFluid } }
-        }
-      }
-    `}
-    render={data => (
-      <Layout>
-        <SEO title="Accommodations" keywords={[]} />
-        <Header photo={data.placeholderImage.childImageSharp.fluid}>Accommodations</Header>
-      </Layout>
-    )}
-  />
+export default ({ data }) => (
+  <Layout>
+    <SEO title="Accommodations" keywords={[]} />
+    <Header photo={data.headerPhoto.childImageSharp.fluid}>Accommodations</Header>
+  </Layout>
 )
 
-export default AccommodationsPage
+export const query = graphql`
+  query {
+    headerPhoto: file(relativePath: { eq: "photos/accommodations.jpg" }) {
+      childImageSharp { fluid { ...GatsbyImageSharpFluid } }
+    }
+  }
+`
+
