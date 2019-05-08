442cee834c5481f4e25592175c7391982273589f
Elaman Imashov <elaman@outlook.com>
2019-05-08 13:59:25 +0600

    Preload fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,40 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react'
+import FS from 'fs'
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  const files = getFilesFromPath("./public/static", ".woff2")
+  const preload = [
+    'eb-garamond-v10-cyrillic_latin-700',
+    'eb-garamond-v10-cyrillic_latin-regular',
+    'subset-TeXGyreAdventor-Bold',
+    'subset-TeXGyreAdventor-Regular',
+  ]
+
+  setHeadComponents([
+    files.map((file) => {
+      return preload.map((fontFileName, key) => {
+        const fileName = file.split('-').slice(0,-1).join('-')
+        if (fileName === fontFileName) {
+          return(
+            <link
+      				key={key}
+      				rel='preload'
+      				as='font'
+      				type='font/woff2'
+      				crossOrigin='anonymous'
+      				href={`/static/${file}`}
+      			/>
+          )
+        }
+      })
+    })
+  ])
+}
+
+function getFilesFromPath(path, extension) {
+  return FS.readdirSync(path)
+    .filter(element => element.match(new RegExp(`.*\.(${extension})`, 'ig')));
+}
